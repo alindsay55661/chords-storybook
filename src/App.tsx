f6da2384e6b942adb13575360b7f282a2f371581
Alan Lindsay <alanlindsay@gmail.com>
2023-01-18 05:11:46 +0000

    make file selector its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-import Dropzone from 'react-dropzone'
 import './App.css'
+import FileSelector from './components/FileSelector'
 import SongCanvas from './components/SongCanvas'
 import { analyzed as data } from './stories/musicData'
-import { analyze, Song, DetectUnit } from './utils/analyze'
-import { parseMidi } from './utils/parse'
+import { Song, DetectUnit } from './utils/analyze'
 
 function App() {
   const [zoom, setZoom] = useState(130)
@@ -12,57 +11,10 @@ function App() {
   const [unit, setUnit] = useState<DetectUnit>('beat')
   const [song, setSong] = useState<Song>(data.chordTest)
 
-  function handleDroppedFiles(files: File[]) {
-    files.forEach(file => {
-      const reader = new FileReader()
-
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
-      reader.onload = () => {
-        if (reader.result) {
-          const result = reader.result as ArrayBufferLike
-          const song = analyze(parseMidi(new Uint8Array(result)))
-          setSong(song)
-        }
-      }
-      reader.readAsArrayBuffer(file)
-    })
-  }
-
   return (
     <div>
-      <h1 className="mb-5 text-xl">
-        Try out midi files from{' '}
-        <a
-          className="text-sky-800 underline"
-          target="_blank"
-          href="https://www.midis101.com/search/Coldplay"
-        >
-          https://www.midis101.com/
-        </a>
-      </h1>
-      <Dropzone
-        onDrop={handleDroppedFiles}
-        maxFiles={1}
-        accept={{
-          'audio/midi': ['.mid', '.midi'],
-        }}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <section>
-            <div
-              className="cursor-pointer rounded border-2 border-dashed border-slate-300 bg-slate-100 p-8"
-              {...getRootProps()}
-            >
-              <input {...getInputProps()} />
-              <p>
-                Drag 'n' drop a <span className="font-bold">".mid"</span> file
-                here, or click to select files
-              </p>
-            </div>
-          </section>
-        )}
-      </Dropzone>
+      <FileSelector setSong={setSong} />
+
       <label className="inline-block p-4">
         <span className="pr-2">Track Width</span>
         <input
@@ -111,6 +63,7 @@ function App() {
           beat
         </label>
       </span>
+      
       <SongCanvas
         song={song}
         chordDetectUnit={unit}
---
+++ src/components/FileSelector.tsx
@@ -0,0 +1,63 @@
+import Dropzone from 'react-dropzone'
+import { analyze } from '../utils/analyze'
+import { parseMidi } from '../utils/parse'
+
+type FileSelectorProps = {
+  setSong: Function
+}
+
+export default function FileSelector({ setSong }: FileSelectorProps) {
+  function handleDroppedFiles(files: File[]) {
+    files.forEach(file => {
+      const reader = new FileReader()
+
+      reader.onabort = () => console.log('file reading was aborted')
+      reader.onerror = () => console.log('file reading has failed')
+      reader.onload = () => {
+        if (reader.result) {
+          const result = reader.result as ArrayBufferLike
+          const song = analyze(parseMidi(new Uint8Array(result)))
+          setSong(song)
+        }
+      }
+      reader.readAsArrayBuffer(file)
+    })
+  }
+
+  return (
+    <>
+      <h1 className="mb-5 text-xl">
+        Try out midi files from{' '}
+        <a
+          className="text-sky-800 underline"
+          target="_blank"
+          href="https://www.midis101.com/search/Coldplay"
+        >
+          https://www.midis101.com/
+        </a>
+      </h1>
+      <Dropzone
+        onDrop={handleDroppedFiles}
+        maxFiles={1}
+        accept={{
+          'audio/midi': ['.mid', '.midi'],
+        }}
+      >
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div
+              className="cursor-pointer rounded border-2 border-dashed border-slate-300 bg-slate-100 p-8"
+              {...getRootProps()}
+            >
+              <input {...getInputProps()} />
+              <p>
+                Drag 'n' drop a <span className="font-bold">".mid"</span> file
+                here, or click to select files
+              </p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
+    </>
+  )
+}
